29242b57901aac89d8557344d36034955afba902
anasalashqar <anasalashqar16@gmail.com>
2025-02-16 09:11:31 +0300

    uploaded Task2

---
+++ Task26(PHP)/LogicalStatements/Question1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task #1</title>
+</head>
+<body>
+   
+
+    <?php 
+        $year = 2013;
+        
+        if (($year % 4 == 0 && $year % 100 != 0) || ($year % 400 == 0)) {
+            echo "This year is a leap year.";
+        } else {
+            echo "This year is not a leap year.";
+        }
+    ?>
+</body>
+</html>
